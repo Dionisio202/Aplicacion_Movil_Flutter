90abda254804a870e618190c694bbe479b50a681
Edison <solisedison@outlook.com>
2023-06-23 08:45:22 -0500

    cosas que no hize segundo sprint

--- lib/alimentacion.dart
+++ lib/alimentacion.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'estadistica.dart';
-import 'menu.dart';
-
 class Alimentacion extends StatefulWidget {
   @override
   _AlimentacionState createState() => _AlimentacionState();
@@ -21,75 +18,14 @@ class _AlimentacionState extends State<Alimentacion> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        // Navega de vuelta al menú al presionar el botón de retroceso
-        setState(() {
-          _selectedIndex = 0;
-        });
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => MyappMenu()),
-        );
-        return false;
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Alimentación"),
-        ),
-        body: Container(
-          // Coloca aquí el contenido de tu widget personalizado
-          child: ElevatedButton(
-            onPressed: _changeColor,
-            style: ElevatedButton.styleFrom(
-              backgroundColor: _buttonColor, // Color del botón
-            ),
-            child: Text('Cambiar Color'),
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(
-                icon: Icon(Icons.home),
-                onPressed: () {
-                  // Acción al presionar el segundo botón
-                  setState(() {
-                    _selectedIndex = 0;
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MyappMenu()),
-                    );
-                  });
-                },
-                color: _selectedIndex == 0 ? Colors.blue : Colors.grey,
-              ),
-              IconButton(
-                icon: Icon(Icons.bar_chart),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => estadistica()),
-                  );
-                  setState(() {
-                    _selectedIndex = 1;
-                  });
-                },
-                color: _selectedIndex == 1 ? Colors.blue : Colors.grey,
-              ),
-              IconButton(
-                icon: Icon(Icons.food_bank_sharp),
-                onPressed: () {
-                  // Acción al presionar el tercer botón
-                  setState(() {
-                    _selectedIndex = 2;
-                  });
-                },
-                color: _selectedIndex == 2 ? Colors.blue : Colors.grey,
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Alimentación'),
+      ),
+      body: Center(
+        child: Text(
+          'Estamos trabajando en esto',
+          style: TextStyle(fontSize: 24),
         ),
       ),
     );
